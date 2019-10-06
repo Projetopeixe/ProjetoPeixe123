bcf579bfc38ef596e414760304bcf94082a570e4
Samuel <samuelamorim865@gmail.com>
2019-10-06 16:46:11 -0300

    Teste

--- app/src/main/java/br/edu/ufopa/cadfishmaster/Teste.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/Teste.java
@@ -1,4 +1,12 @@
 package br.edu.ufopa.cadfishmaster;
 
+import androidx.annotation.NonNull;
+
 public class Teste {
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
