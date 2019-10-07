8ec0b73c5adc87c9a2808c7066a6a6928cf845d8
Luis Vitor <vitinhoveras@gmail.com>
2019-10-07 15:21:15 -0300

    Teste

--- app/src/main/java/br/edu/ufopa/cadfishmaster/activity/ActivityCarregamento.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/ActivityCarregamento.java
@@ -13,4 +13,6 @@ public class ActivityCarregamento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carregamento);
     }
+
+    //jahfSFGWAXDH 
 }
