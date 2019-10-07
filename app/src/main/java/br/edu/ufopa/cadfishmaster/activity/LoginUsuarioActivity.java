ba99b9fe9c220f8a1447b5c7b44c32ca7d6a44b4
Samuel <samuelamorim865@gmail.com>
2019-10-07 14:29:36 -0300

    Actvitys
    Signed-off-by: Samuel <samuelamorim865@gmail.com>

---
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/LoginUsuarioActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ufopa.cadfishmaster.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ufopa.cadfishmaster.R;
+
+public class LoginUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_usuario);
+    }
+}
