2c192dccbf4748af12f0ef2ca02d5ecb946b3f5c
Samuel <samuelamorim865@gmail.com>
2019-10-15 18:13:20 -0300

    Cadastro de Peixe - Passo 2

--- app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_peixe/CadastroDePeixePasso2.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_peixe/CadastroDePeixePasso2.java
@@ -2,15 +2,25 @@ package br.edu.ufopa.cadfishmaster.activity.cadastro_peixe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import br.edu.ufopa.cadfishmaster.R;
+import br.edu.ufopa.cadfishmaster.activity.cadastro_especies.CadastroDeEspeciesPasso1;
 
 public class CadastroDePeixePasso2 extends AppCompatActivity {
 
     private Button buttonNext;
     private Button buttonBack;
+    private TextInputEditText campoPeso;
+    private TextInputEditText campoTamanho;
+    private TextInputEditText campoTag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +29,37 @@ public class CadastroDePeixePasso2 extends AppCompatActivity {
 
         buttonNext = (Button) findViewById(R.id.buttonNextPasso2);
         buttonBack = (Button) findViewById(R.id.buttonBackPasso2);
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String peso = campoPeso.getText().toString();
+                String tamanho = campoTamanho.getText().toString();
+                String tag = campoTag.getText().toString();
+
+                if(!peso.isEmpty()){
+                    if(!tamanho.isEmpty()){
+                        if(!tag.isEmpty()){
+
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Informe a Marca Tag", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Informe o tamanho", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "Informe o peso", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CadastroDeEspeciesPasso1.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
