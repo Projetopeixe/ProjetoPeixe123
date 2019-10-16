9c135337a44e5b30c189aea05c919047b7e7f953
Samuel <samuelamorim865@gmail.com>
2019-10-16 15:54:07 -0300

    Utilizando Câmera e Armazenamento interno

--- app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_peixe/CadastroDePeixePasso3.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_peixe/CadastroDePeixePasso3.java
@@ -1,9 +1,13 @@
 package br.edu.ufopa.cadfishmaster.activity.cadastro_peixe;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +17,8 @@ public class CadastroDePeixePasso3 extends AppCompatActivity {
 
     private Button buttonNext;
     private Button buttonBack;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +46,42 @@ public class CadastroDePeixePasso3 extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void carregarArmazenamento(View view){
+
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (i.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(i, SELECAO_GALERIA);
+        }
+    }
+
+    public void carregarCamera(View view){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (i.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(i, SELECAO_CAMERA);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+            try {
+                switch (requestCode){
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap)data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem =  MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
