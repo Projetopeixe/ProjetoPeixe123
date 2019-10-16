9ac5a3f1afc87d5fe5de0d210c2e30fe4adfd93b
Samuel <samuelamorim865@gmail.com>
2019-10-16 17:36:59 -0300

    Nova Activity

---
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_especies/CadastroDeEspeciesPasso2.java
@@ -0,0 +1,16 @@
+package br.edu.ufopa.cadfishmaster.activity.cadastro_especies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ufopa.cadfishmaster.R;
+
+public class CadastroDeEspeciesPasso2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_de_especies_passo2);
+    }
+}
