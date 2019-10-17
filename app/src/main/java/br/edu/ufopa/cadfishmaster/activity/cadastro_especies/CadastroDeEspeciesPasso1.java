66b2d20cb4adea43a2ed2d142eccf2e3bfab786d
Projeto Peixe <peixeprojeto@gmail.com>
2019-10-17 17:05:48 -0300

    Interação de tela

--- app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_especies/CadastroDeEspeciesPasso1.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_especies/CadastroDeEspeciesPasso1.java
@@ -2,15 +2,30 @@ package br.edu.ufopa.cadfishmaster.activity.cadastro_especies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.edu.ufopa.cadfishmaster.R;
 
 public class CadastroDeEspeciesPasso1 extends AppCompatActivity {
+    private Button  buttonNextespecie;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_de_especies_passo1);
+        buttonNextespecie = findViewById(R.id.buttonNextespecie);
+        buttonNextespecie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CadastroDeEspeciesPasso2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
