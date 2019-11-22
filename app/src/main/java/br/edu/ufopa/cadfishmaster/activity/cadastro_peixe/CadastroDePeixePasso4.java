912ffde0e00eb30e0b2fc5677637c713e52fd178
Luis Vitor <vitinhoveras@gmail.com>
2019-11-22 15:29:35 -0300

    Merge branch 'master' of C:\Users\Marcelo Alves\AndroidStudioProjects\ProjetoPeixe123 with conflicts.

--- app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_peixe/CadastroDePeixePasso4.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/cadastro_peixe/CadastroDePeixePasso4.java
@@ -6,24 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import br.edu.ufopa.cadfishmaster.R;
-import br.edu.ufopa.cadfishmaster.config.ConfiguracaoDB;
 
 public class CadastroDePeixePasso4 extends AppCompatActivity {
 
-    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
     private Button finalizar;
-    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-            .setPersistenceEnabled(true)
-            .build();
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,37 +23,12 @@ public class CadastroDePeixePasso4 extends AppCompatActivity {
         getSupportActionBar().setTitle("Cadastro de Peixe");
 
         finalizar = (Button) findViewById(R.id.buttonFinalizarCadPeix);
-        Bundle dados = getIntent().getExtras();
-        if(dados != null){
-            String especie = dados.getString("especie");
-            Double peso = dados.getDouble("peso");
-            Double tamanho = dados.getDouble("tamanho");
-            String tag = dados.getString("tag");
-
-            String localizacao = dados.getString("localizacao");
-            Map<String, Object> docData = new HashMap<>();
-            docData.put("especie", especie);
-            docData.put("peso", peso);
-            docData.put("tamanho", tamanho);
-            docData.put("tag", tag);
-            docData.put("localizacao", localizacao);
-
-            ConfiguracaoDB configuracaoDB = new ConfiguracaoDB();
-            configuracaoDB.saveNote(docData);
-        }
-
-
-
-
 
         finalizar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-
             }
         });
-
     }
-
 }
