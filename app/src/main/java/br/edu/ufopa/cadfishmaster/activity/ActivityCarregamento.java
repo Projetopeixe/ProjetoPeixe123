b9f88a5db68ccf24ad4e1233f324559ad480fa6b
Samuel <samuelamorim865@gmail.com>
2019-12-26 16:13:17 -0300

    Changes in register fishes

--- app/src/main/java/br/edu/ufopa/cadfishmaster/activity/ActivityCarregamento.java
+++ app/src/main/java/br/edu/ufopa/cadfishmaster/activity/ActivityCarregamento.java
@@ -3,6 +3,7 @@ package br.edu.ufopa.cadfishmaster.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -18,9 +19,20 @@ public class ActivityCarregamento extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                try{
+                    SQLiteDatabase banco = openOrCreateDatabase("cadFishapp", MODE_PRIVATE, null);
+                    //Create Table de Usuários
+                   //banco.execSQL("CREATE TABLE IF NOT EXISTS usuarios()");
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
                 abriTelaInicialLogin();
             }
         }, 3000);
+
     }
 
 
